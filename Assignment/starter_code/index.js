6faa77d5fc835ab6ad08a6bee1c0946e8b6b139b
JorHernandez <jorgeh@broadvoice.com>
2018-05-15 14:03:38 -0700

    Slide
    Sliding like a power penguin

---
+++ Assignment/starter_code/index.js
@@ -0,0 +1,41 @@
+// function toggleReadMore() {
+
+//     $('#show-this-on-click').slideToggle();
+// }
+// $('.readmore').click(toggleReadMore);
+
+// function toggleLearnMore() {
+//     $("#learnmoretext").slideToggle();
+// }
+
+// $(".learnmore").click(toggleLearnMore)
+
+
+
+// $(".readmore").click(toggleReadMore);
+
+
+
+// $(function (){
+//     event.preventDefault();
+// });
+
+//readmored -> show-this-on-click
+
+$(function(){
+    $(".readmore").click(function(e){
+        e.preventDefault();
+        $("#show-this-on-click").slideDown();
+            $(".readless").slideDown();
+                $(".readmore").hide();
+    })
+})
+
+$(function(){
+    $(".learnmore").click(function(e){
+        e.preventDefault();
+        $("#learnmoretext").slideDown();
+    })
+})
+
+
